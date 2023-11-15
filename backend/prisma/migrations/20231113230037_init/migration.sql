52814aee7c7d71be4d69208f7e99abbc5e112361
afiorenza <agustinfiorenza@gmail.com>
2023-11-15 11:13:00 +0100

    Remove migrations from staged folder

--- backend/prisma/migrations/20231113230037_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Metrics" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "timestamp" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "value" REAL NOT NULL
-);
